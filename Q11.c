3f52dc174ded7bddde25696c1fc66a770c36e0ec
Abhimanyu Garg <abhimanyu3062008@gmail.com>
2026-09-09 05:55:25 +0000

    Update Q11.c

--- Q11.c
+++ Q11.c
@@ -2,7 +2,21 @@
 
 int main(){
 
+int a;
 
+printf("Enter a Number : ");
+scanf("%d", &a);
+
+if(a%2==0){
+
+printf("The number is even \n");
+
+}
+
+else{
+
+printf("The number is odd \n");
+}
 
 
 
